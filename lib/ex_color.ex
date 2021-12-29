899b206e5d632dafe79841477f39249c84355fa6
EVOKE <evoke.lektrique@gmail.com>
2021-12-29 18:45:32 +0330

    New changes

--- lib/ex_color.ex
+++ lib/ex_color.ex
@@ -1,77 +1,50 @@
 defmodule ExColor do
-  defstruct [:width, :height, :bit_depth, :color_type, :compression, :filter, :interlace, :chunks]
+  defstruct [:width, :height, :pixels]
 
-  def read(path) do
-    path
-    |> File.read!()
-    |> parse_png()
+  def extract(path) do
+    {:ok, pixels} = Pixels.read_file(path)
+
+    pixels
+  end
+end
+
+defmodule ExColor.Pixel do
+  defstruct [:x, :y, :color]
+
+  alias __MODULE__
+  alias ExColor.Color
+
+  def get_pixels(image, x, y) do
+    offset = pixel_offset(image, x, y)
+    <<_::binary-size(offset), pixel::binary-size(4), _::binary>> = image.data
+
+    # Color.get_color(pixel)
+    %Pixel{x: x, y: y, color: Color.get_color(pixel)}
   end
 
-  def parse_png(binary) do
-    <<
-      # According to the libpng table reference: 
-      # http://www.libpng.org/pub/png/book/chapter08.html#png.ch08.tbl.1
-      137,
-      ?P,
-      ?N,
-      ?G,
-      13,
-      10,
-      26,
-      10,
-      _length::unsigned-32,
-      ?I,
-      ?H,
-      ?D,
-      ?R,
-      width::unsigned-32,
-      height::unsigned-32,
-      bit_depth,
-      color_type,
-      compression_method,
-      filter_method,
-      interlace_method,
-      _crc::unsigned-32,
-      chunks::binary
-    >> = binary
+  def pixel_offset(image, x, y) do
+    (y * image.width + x) * 4
+  end
+end
 
-    png = %ExColor{
-      width: width,
-      height: height,
-      bit_depth: bit_depth,
-      color_type: color_type,
-      compression: compression_method,
-      filter: filter_method,
-      interlace: interlace_method,
-      chunks: []
+defmodule ExColor.Color do
+  defstruct [:red, :green, :blue, alpha: 255]
+
+  alias __MODULE__
+
+  def get_color(<<r, g, b, a>>), do: %Color{red: r, green: g, blue: b, alpha: a}
+
+  def get_color(<<r, g, b>>) do
+    %Color{
+      rgb: [red: r, green: g, blue: b],
+      hex: to_hex()
     }
-
-    parse_png_chunks(chunks, png)
   end
 
-  defp parse_png_chunks(
-         <<
-           length::unsigned-32,
-           chunk_type::unsigned-32,
-           chunk_data::binary-size(length),
-           crc::unsigned-32,
-           chunks::binary
-         >>,
-         png
-       ) do
-    chunk = %{
-      length: length,
-      chunk_type: :binary.encode_unsigned(chunk_type),
-      data: chunk_data,
-      crc: crc
-    }
-
-    png = %{png | chunks: [chunk | png.chunks]}
-
-    parse_png_chunks(chunks, png)
+  # TODO:
+  def to_rgb(<<r, g, b>>) do
+    %{red: r, green: g, blue: blue}
   end
 
-  defp parse_png_chunks(<<>>, png) do
-    %{png | chunks: Enum.reverse(png.chunks)}
-  end
+  def to_hex(<<r, g, b, _>>), do: to_hex(<<r, g, b>>)
 end
