f32eb542a698c5e46ed528853574cee41c0fb8a7
EVOKE <evoke.lektrique@gmail.com>
2021-12-31 15:43:07 +0330

    Add `color_distance` function

--- lib/ex_color.ex
+++ lib/ex_color.ex
@@ -3,6 +3,7 @@ defmodule ExColor do
   TODO: Write documentation
   """
 
+  use Bitwise
   alias ExColor.Pixel
   alias ExColor.Color
 
@@ -25,5 +26,20 @@ defmodule ExColor do
     |> Enum.sort(fn {_k1, value_1}, {_k2, value_2} -> value_1 >= value_2 end)
     |> Enum.take(quantity)
     |> Enum.map(fn {color, _count} -> color end)
+  end
+
+  #
+  # Distance between two RGB colors
+  #
+  # Example:
+  #   iex> ExColor.color_distance({0, 0, 0}, {1, 1, 0})
+  #   2.449489742783178
+  #
+  def color_distance({r1, g1, b1} = _color_1, {r2, g2, b2} = _color_2) do
+    rmean = trunc((r1 + r2) / 2)
+    r = r1 - r2
+    g = g1 - g2
+    b = b1 - b2
+    :math.sqrt((((512 + rmean) * r * r) >>> 8) + 4 * g * g + (((767 - rmean) * b * b) >>> 8))
   end
 end
